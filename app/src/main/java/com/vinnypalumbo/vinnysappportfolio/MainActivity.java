00a227310ba4157daff24ad83704bf7059bb73b9
Vinny <vin_palumbo@hotmail.com>
2015-11-26 13:38:07 -0500

    Remove Menu with Settings button

--- app/src/main/java/com/vinnypalumbo/vinnysappportfolio/MainActivity.java
+++ app/src/main/java/com/vinnypalumbo/vinnysappportfolio/MainActivity.java
@@ -5,8 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,28 +18,6 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setBackgroundColor(Color.BLUE);
         setSupportActionBar(toolbar);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void popularMoviesClicked(View view) {
